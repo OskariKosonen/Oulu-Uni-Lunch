b2edad6829684c19a3e081600aac4fa24fea7784
sfosko <oskari.kosonen@tuni.fi>
2024-12-02 13:12:29 +0200

    add menuService

---
+++ src/api/menuService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const fetchMenu = async (customerID, kitchenID) => {
+  const API_URL = `https://fi.jamix.cloud/apps/menuservice/rest/haku/menu/${customerID}/${kitchenID}?lang=fi`;
+
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching menu:', error);
+    throw error;
+  }
+};
